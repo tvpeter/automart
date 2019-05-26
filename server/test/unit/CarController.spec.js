8bfbc3f735585c2d7bb92c69a129acdf0eea4af3
tvpeter <withtvpeter@gmail.com>
2019-05-26 06:01:32 +0100

    test case when there are no unsold cars found

--- server/test/unit/CarController.spec.js
+++ server/test/unit/CarController.spec.js
@@ -1,5 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import carsData from '../../carsData';
 import server from '../../index';
 import Cars from '../../models/CarModel';
 
@@ -8,10 +9,14 @@ chai.use(chaiHttp);
 const adUrl = '/api/v1/car';
 describe('Cars', () => {
   let token;
+  const carsArray = () => {
+    Cars.cars = carsData;
+  };
   beforeEach(() => {
-    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTU1ODc2OTc3NDQ5NCwicm9sZSI6ZmFsc2UsImlhdCI6MTU1ODc2OTcxMiwiZXhwIjoxNTU4ODEyOTEyfQ.GcU_qJiP9A8Y65c8C4LBYzlDgQeh8P7vAe7p2JyMSHg';
+    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTU1ODgyMTgwMDI5OCwicm9sZSI6ZmFsc2UsImlhdCI6MTU1ODgyMTcyNCwiZXhwIjoxNTU4ODY0OTI0fQ.SkdISgxuJXDme3k9cOH6gRQMKrykLhDiZy5c-IjoonE';
   });
   afterEach(() => {
+    Cars.cars = [];
     token = '';
   });
   describe('Create Ad', () => {
@@ -39,7 +44,7 @@ describe('Cars', () => {
     it('should return error 400 if request does not contain all required fields', (done) => {
       const data = {
         owner: 'owener',
-        status: 'avaialable',
+        status: '',
         price: '2.5m',
         state: 'new',
         manufacturer: '',
@@ -57,28 +62,19 @@ describe('Cars', () => {
         });
     });
     it('should return error 409 if user has the same car that is available', () => {
-      Cars.createCar({
-        owner: '1558605162264',
-        status: 'avaialable',
-        price: '2.5m',
-        state: 'new',
-        model: 'es6 v',
-        manufacturer: 'BMW',
-        body_type: 'car',
-        description: 'The car is still new',
-        img: 'https://mydummyimgurl.com',
-      });
-
+      carsArray();
       const data = {
-        owner: '1558605162264',
-        status: 'avaialable',
-        price: '2.5m',
-        state: 'new',
-        model: 'es6 v',
-        manufacturer: 'BMW',
+        id: 1558731168820,
+        owner: 1558730737306,
+        created_on: '5/24/2019, 9:51:34 PM',
+        state: 'New',
+        status: 'available',
+        price: '12000000',
+        manufacturer: 'AUDI',
+        model: 'SPORT UV',
         body_type: 'car',
-        description: 'The car is still new',
-        img: 'https://mydummyimgurl.com',
+        description: 'This is the description of the car',
+        img: 'http://res.cloudinary.com/tvpeter/image/upload/v1558731093/vkjzwklvedrocyyerzyr.jpg',
       };
       chai.request(server).post(adUrl).set('x-auth', token)
         .send(data)
@@ -134,6 +130,7 @@ describe('Cars', () => {
 
   describe('view unsold cars by manufacturer', () => {
     it('should return all unsold cars by a manufacturer', (done) => {
+      carsArray();
       const manufacturers = [
         'BMW', 'TOYOTA', 'NISSAN',
       ];
@@ -146,6 +143,7 @@ describe('Cars', () => {
     });
 
     it('should return a custom error if no vehicle is found for the manufacturer', (done) => {
+      carsArray();
       const manufacturers = [
         'BMW', 'TOYOTA', 'NISSAN',
       ];
@@ -160,9 +158,20 @@ describe('Cars', () => {
   // view all unsold cars
   describe('view all unsold cars', () => {
     it('should return all unsold cars', (done) => {
+      carsArray();
       chai.request(server).get('/api/v1/cars/status/available').end((err, res) => {
         expect(res.status).to.eq(200);
         expect(res.body).to.have.property('data').to.be.an('ARRAY');
+        done();
+      });
+    });
+    it('should return 404 when there are no unsold cars', (done) => {
+      before(() => {
+        Cars.cars = [];
+      });
+      chai.request(server).get('/api/v1/cars/status/available').end((err, res) => {
+        expect(res.status).to.eq(404);
+        expect(res.body.error).to.eq('There are no cars available now. Check back');
         done();
       });
     });

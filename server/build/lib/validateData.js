6b6fc5c04b513cdee5319befb7cf02c9602579dc
tvpeter <withtvpeter@gmail.com>
2019-06-14 01:01:32 +0100

    chore(code refactor): Remove build directory from git cached

--- server/build/lib/validateData.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-const validateData = (requiredProperties, data) => requiredProperties.find(property => data[property] === undefined || data[property] === '');
-exports.default = validateData;

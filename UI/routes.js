dc39699a24da03f3a560bd97803aad63ac24cc16
tvpeter <withtvpeter@gmail.com>
2019-06-30 10:33:12 +0100

    feature(user signup integration): add user signup DOM and db interaction

---
+++ UI/routes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path';
+
+const router = express.Router();
+
+router.get('/', (req, res) => res.status(200).sendFile(path.join(`${__dirname}/index.html`)));
+router.get('/signup', (req, res) => res.status(200).sendFile(path.join(`${__dirname}/signup.html`)));
+router.get('/signin', (req, res) => res.status(200).sendFile(path.join(`${__dirname}/login.html`)));
+router.get('/details', (req, res) => res.status(200).sendFile(path.join(`${__dirname}/details.html`)));
+router.get('/products', (req, res) => res.status(200).sendFile(path.join(`${__dirname}/products.html`)));
+router.get('/contact', (req, res) => res.status(200).sendFile(path.join(`${__dirname}/contact.html`)));
+router.get('/userprofile', (req, res) => res.status(200).sendFile(path.join(`${__dirname}/userprofile.html`)));
+
+export default router;

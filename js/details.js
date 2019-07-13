256067f7a3bcc868b9bb98bb9d0a967a08eef670
tvpeter <withtvpeter@gmail.com>
2019-07-13 12:11:39 +0100

    chore (Order integration): add user make an order integration

--- js/details.js
+++ js/details.js
@@ -1,17 +1,30 @@
-/* eslint-disable consistent-return */
+const productId = window.location.href.split('=')[1];
 const errorDisplay = document.querySelector('#errorDisplay');
-window.addEventListener('DOMContentLoaded', async (e, id) => {
+
+// eslint-disable-next-line consistent-return
+window.addEventListener('DOMContentLoaded', async (e) => {
   e.preventDefault();
 
   try {
-    const adDetail = await fetch(`http://localhost:5000/api/v1/car/${id}`, {
+    const adDetail = await fetch(`http://localhost:5000/api/v1/car/${productId}`, {
       method: 'get',
       headers: {
         'Content-Type': 'application/json',
       },
     });
-
-    console.log(adDetail);
+    const responseToJson = await adDetail.json();
+    if (responseToJson.status === 200) {
+      const { data } = responseToJson;
+      const manufacturer = data.manufacturer.toUpperCase();
+      document.querySelector('.imgslide').src = data.img;
+      document.querySelector('.col-55 h1').textContent = `${manufacturer}  ${data.model}`;
+      document.querySelector('.vehicle-detail-info').textContent = data.description;
+      document.querySelector('.vehicle-info p').textContent = `Price: N${data.price}`;
+      document.querySelector('.vehicle-info span').textContent = data.state;
+      const buyButton = document.querySelector('.txt-center a');
+      buyButton.href = `./order.html?pid=${productId}`;
+    }
+    errorDisplay.textContent = responseToJson.message;
   } catch (error) {
     const message = 'There\'s an error loading your data, please retry';
     errorDisplay.textContent = message;
